ddc1a851ac8a1b587a0ceb81fa2cc4d471e1e834
Lakr <25259084+Lakr233@users.noreply.github.com>
2024-07-04 02:42:52 +0800

    Update build.xcframework.sh

--- Scripts/build.xcframework.sh
+++ Scripts/build.xcframework.sh
@@ -24,7 +24,7 @@ mkdir -p $OUTPUT_DIR || true
 declare -A FRAMEWORK_DEFS
 FRAMEWORK_DEFS["libssl"]="openssl|libcrypto.a libssl.a"
 FRAMEWORK_DEFS["libplist"]="plist/plist.h|libplist-2.0.a"
-FRAMEWORK_DEFS["libtatsu"]="tatsu/tatsu.h tatsu/tss.h|libtatsu-1.0.a"
+FRAMEWORK_DEFS["libtatsu"]="libtatsu/tatsu.h libtatsu/tss.h|libtatsu.a"
 FRAMEWORK_DEFS["libimobiledevice_glue"]="libimobiledevice-glue|libimobiledevice-glue-1.0.a"
 FRAMEWORK_DEFS["libusbmuxd"]="usbmuxd-proto.h usbmuxd.h|libusbmuxd-2.0.a"
 FRAMEWORK_DEFS["libimobiledevice"]="libimobiledevice|libimobiledevice-1.0.a"
